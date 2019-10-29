a293e0bb8a4a31c25771e639f2c0100410b35b9a
Sarath Sreedharan (Student) <ASUAD\ssreedh3@en4119722l.fulton.ad.asu.edu>
2019-10-28 21:50:06 -0700

    Adding domain only files

--- src/PDDLhelp.py
+++ src/PDDLhelp.py
@@ -36,6 +36,139 @@ def get_problem_state_preds(domain_file, proble_file, section_prefix):
 Method :: write domain file from given state
 '''
 
+def write_domain_only_file_from_state(state, domain_source, dom_dst=None):
+
+    predicateList = set()
+    actionList    = {}
+    init_state_list = []
+    goal_state_list =[]
+
+    for item in state:
+        if "state" not in item:
+            regex_probe   = re.compile("(.*)-has-(parameters|negprecondition|precondition|add-effect|delete-effect)-(.*)$").search(item)
+            actionName    = regex_probe.group(1)
+            _condition    = regex_probe.group(2)
+            predicateName = regex_probe.group(3)
+
+            predicateList.add(predicateName)
+
+            if actionName not in actionList: actionList[actionName] = {'parameters':"", 'precondition' : [], 'negprecondition' : [], 'add-effect' : [], 'delete-effect' : []}
+            if _condition == 'parameters':
+                actionList[actionName][_condition] = predicateName
+            else:
+                actionList[actionName][_condition].append(predicateName) 
+        else:
+            regex_probe   = re.compile("has-(initial|goal)-state-(.*)$").search(item)
+            state_type = regex_probe.group(1)
+            pred = regex_probe.group(2)
+            if state_type == 'initial':
+                init_state_list.append('({})'.format(' '.join(pred.split('@'))))
+            else:
+                goal_state_list.append('({})'.format(' '.join(pred.split('@'))))
+    if not dom_dst:
+        temp_domainFileName = 'temp.pddl'
+    else:
+        temp_domainFileName = dom_dst
+
+    with open(domain_source, 'r') as template_domain_file:
+        template_domain = template_domain_file.read()
+
+
+
+
+    with open(temp_domainFileName, 'w') as temp_domain_file:
+
+        predicateString = '\n'.join(['( {} )'.format(item) if item != '' else '' for item in predicateList])
+        actionString    = '\n'.join(['(:action {}\n:parameters ({})\n:precondition\n(and\n{}\n)\n:effect\n(and\n{}\n)\n)\n'\
+                                     .format(key, actionList[key]['parameters'],'\n'.join(['( {} )'.format(p) for p in actionList[key]['precondition']]) + '\n'+
+                                             '\n'.join(['(not ( {} ) )'.format(p) for p in actionList[key]['negprecondition']]), \
+                                             '{}\n{}'.format('\n'.join(['( {} )'.format(p) for p in actionList[key]['add-effect']]), \
+                                                             '\n'.join(['(not ( {} ))'.format(p) for p in actionList[key]['delete-effect']]))) for key in actionList.keys()])
+
+        template_domain = template_domain.replace('%PREDICATES%',predicateString)
+        temp_domain_file.write(template_domain.replace('%OPERATORS%',actionString))
+
+    return temp_domainFileName
+        
+
+'''
+Method :: read state from given domain file
+'''
+
+def read_state_from_domain_only_file(domainFileName):
+
+    def PDDLaction(description):
+        action_name            = re.search('\(:action(.*?)[\s+]*:', description).group(1).strip()
+
+        try:    parameters     = re.search(':parameters[\s+]*\((.*?)\)[\s+]*:', description).group(1)
+        except: parameters     = ""
+        try:    preconditions  = {re.search('\(((?!not).*?)\)', item).group(1).strip() : not 'not ' in item \
+                                  for item in re.findall('(\(not[\s+]*\(.*?\)[\s+]*\)|\(.*?\))',
+                                                         re.search(':precondition[\s+]*\(and(.*?)\)[\s+]*:',
+                                                                   description).group(1))}
+
+        except: preconditions  = {}
+        try:
+            #all_effects = [item for item in
+            #               re.findall('(\(not[\s+]*\(.*?\)[\s+]*\)|\(increase[\s+]*\(.*?\)[\s+]*\(.*?\)[\s+]*\)|\(.*?\))',
+            #                                           re.search(':effect[\s+]*\(and(.*?)\)$', description).group(1))]
+
+            #effects = {}
+            #for eff in all_effects:
+            #    if 'increase' in eff:
+            #        effects[re.search('\((increase.*)\)',eff).group(1)] = True
+            #    else:
+            #        effects[re.search('\(((?!not\s).*?)\)', eff).group(1).strip()] = bool(not 'not ' in eff)
+            #effects = {re.search('\(((?!not).*?)\)', item).group(1).strip(): not 'not ' in item \
+            #          for item in re.findall('(\(not[\s+]*\(.*?\)[\s+]*\)|\(.*?\))',
+            #                                  re.search(':effect[\s+]*\(and(.*?)\)[\s+]*(\(:action|\)[\s+]*$)',
+            #                                            description).group(1))}
+
+            effects = {re.search('\(((?!not).*?)\)', item).group(1).strip(): not 'not ' in item \
+                       for item in re.findall('(\(not[\s+]*\(.*?\)[\s+]*\)|\(.*?\))',
+                                              re.search(':effect[\s+]*\(and(.*?)\)[\s+]*(\(:action|\)[\s+]*$)',
+                                                        description).group(1))}
+
+        except Exception as exc:
+            print "exception",exc
+            #print description
+            effects        = {}
+            
+        return [action_name, parameters, preconditions, effects]
+
+    
+    ''''''
+
+    with open(domainFileName, 'r') as domain_file:
+        action_dict = {item.split(' ')[1] : PDDLaction(item)
+                       for item in re.findall('\(:action.*?:effect.*?[not.*?\)]*[\s+]*\)[\s+]*\)[\s+]*\)', re.sub('[\s+]', ' ', domain_file.read()))}
+
+        #print action_dict
+    #with open(problemFileName) as problem_file:
+    #    problem_lines = problem_file.readlines()
+        #print init_state
+        #print goal_state
+    ''''''
+
+    state = []
+    for key in action_dict.keys():
+        actionName        = action_dict[key][0]
+        state.append('{}-has-parameters-{}'.format(actionName, action_dict[key][1]))
+        for precondition in action_dict[key][2].items():
+            if precondition[1]:
+                state.append('{}-has-precondition-{}'.format(actionName, precondition[0]))
+            else:
+                state.append('{}-has-negprecondition-{}'.format(actionName, precondition[0]))
+           
+        for effect in action_dict[key][3].items():
+            if effect[1]: state.append('{}-has-add-effect-{}'.format(actionName, effect[0]))
+            else:         state.append('{}-has-delete-effect-{}'.format(actionName, effect[0]))
+    return state
+
+
+
+
+
 def write_domain_file_from_state(state, domain_source, problem_source, dom_dst=None, prob_dst=None):
 
     predicateList = set()
